fa7b374945166aa8970a5a2d47faf5c486441feb
ihassaan4 <hassaan.habib@yahoo.com>
2020-03-10 10:09:07 +0500

    Initital Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="form">
+
+<h1>Student Registration</h1>
+
+<div id="inputs">
+<h2>Form</h2>
+<form method="POST">
+<p>Student Name</p>
+<input type="text" name="studentName" placeholder="Enter full name"/>
+<p>Father Name</p>
+<input type="text" name="fatherName" placeholder="Enter full name"/>
+<p>Email</p>
+<input type="email" name="email" placeholder="Enter email"/><br>
+<input type="submit" name="submit"/>
+</form>
+</div>
+
+</div>
+</body>
+</html>
+	
